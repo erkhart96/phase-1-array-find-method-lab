5f6d9f7f096f8fc737ba9be813115b2849e13e43
Ben <berkhart@rocketmail.com>
2022-08-12 12:41:06 -0500

    commit tree 13cb8b6e8839ec4791646297df35ff5a21608189

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+    function superbowlWin(record) {
+        const win = record.find( e => e.result === "W")
+        if (win) {
+          return win.year
+        } else {
+          return undefined
+        }
+      };
